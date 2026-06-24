0318ae10fbefafd3890092fdc655f6d4198f20cc
polleen25-oss <polleen25@gmail.com>
2026-06-24 20:04:27 +0700

    add google verify file

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1858534555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2253410453" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
